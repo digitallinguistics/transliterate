9fee152646f6977d09d6dce65a125da02c01683e
dwhieb <dwhieb@gmail.com>
2019-01-26 18:53:14 -0600

    add upload script
    closes #12

---
+++ build/upload.js
@@ -0,0 +1,29 @@
+const azure         = require(`azure-storage`);
+const credentials   = require(`./credentials`);
+const { version }   = require(`../package.json`);
+const path          = require(`path`);
+const { promisify } = require(`util`);
+
+const storage    = azure.createBlobService(credentials);
+const uploadFile = promisify(storage.createBlockBlobFromLocalFile).bind(storage);
+
+void async function upload() {
+
+  const options = { contentSettings: { contentType: `application/javascript` } };
+
+  const srcFilePath    = path.join(__dirname, `../transliterate.js`);
+  const bundleFilePath = path.join(__dirname, `../transliterate.bundle.js`);
+
+  try {
+    await uploadFile(`scripts`, `transliterate.js`, srcFilePath, options);
+    await uploadFile(`scripts`, `transliterate-latest.js`, srcFilePath, options);
+    await uploadFile(`scripts`, `transliterate-${version}.js`, srcFilePath, options);
+    await uploadFile(`scripts`, `transliterate.bundle.js`, bundleFilePath, options);
+    await uploadFile(`scripts`, `transliterate.bundle-latest.js`, bundleFilePath, options);
+    await uploadFile(`scripts`, `transliterate.bundle-${version}.js`, bundleFilePath, options);
+  } catch (e) {
+    throw e;
+  }
+
+
+}();
