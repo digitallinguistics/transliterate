616a5a0d2cc59bf9b19cc2d11bb49edbbc488d91
Daniel W. Hieber <dwhieb@gmail.com>
2024-05-30 17:28:36 -0500

    DEV: Lint code

--- transliterate.js
+++ transliterate.js
@@ -1,5 +1,6 @@
 /* eslint-disable
   no-constructor-return,
+  no-use-before-define,
 */
 
 /**
@@ -8,9 +9,9 @@
  * @return {String} Returns the random Unicode character
  */
 function getRandomCodePoint() {
-  const blockBegin  = 9632;
-  const blockLength = 95;
-  return String.fromCodePoint(Math.floor(Math.random() * blockLength) + blockBegin);
+  const blockBegin  = 9632
+  const blockLength = 95
+  return String.fromCodePoint(Math.floor(Math.random() * blockLength) + blockBegin)
 }
 
 /**
@@ -18,7 +19,7 @@ function getRandomCodePoint() {
  * @see transliterate
  */
 function sanitize(string, substitutions) {
-  return transliterate(string, substitutions);
+  return transliterate(string, substitutions)
 }
 
 /**
@@ -27,8 +28,8 @@ function sanitize(string, substitutions) {
  */
 class Sanitizer {
   constructor(substitutions) {
-    this.substitutions = substitutions;
-    return string => sanitize(string, substitutions);
+    this.substitutions = substitutions
+    return string => sanitize(string, substitutions)
   }
 }
 
@@ -52,22 +53,22 @@ function transliterate(string = ``, substitutions = {}) {
   // Type Checking
 
   if (typeof string !== `string`) {
-    throw new TypeError(`The first argument passed to the transliterate function must be a string.`);
+    throw new TypeError(`The first argument passed to the transliterate function must be a string.`)
   }
 
   if (!(
     typeof substitutions === `object`
     && Object.values(substitutions).every(val => typeof val === `string`)
   )) {
-    throw new TypeError(`The second argument passed to the transliterate function must be an object whose attributes and values are both strings.`);
+    throw new TypeError(`The second argument passed to the transliterate function must be an object whose attributes and values are both strings.`)
   }
 
   // Variables
 
-  const inputs = Object.keys(substitutions); // a list of the inputs
-  const subs   = new Map;                    // holds the set of substitutions that need to be made
-  const temps  = new Map;                    // tracks of any temporary placeholders
-  let str      = string;                     // the string to manipulate
+  const inputs = Object.keys(substitutions) // a list of the inputs
+  const subs   = new Map                    // holds the set of substitutions that need to be made
+  const temps  = new Map                    // tracks of any temporary placeholders
+  let str      = string                     // the string to manipulate
 
   // Transliteration Steps
 
@@ -75,48 +76,48 @@ function transliterate(string = ``, substitutions = {}) {
   Object.entries(substitutions)
 
   // sort the substitutions by length of the input (avoids partial replacements)
-  .sort(([a], [b]) => b.length - a.length)
+    .sort(([a], [b]) => b.length - a.length)
 
   // make each substitution on the string, using temporary placeholders if needed
-  .forEach(([input, replacement]) => {
+    .forEach(([input, replacement]) => {
 
-    // escape regexp special characters in the input
-    const escapedInput = input.replace(/[.*+?^${}()|[\]\\]/gu, `\\$&`);
+      // escape regexp special characters in the input
+      const escapedInput = input.replace(/[.*+?^${}()|[\]\\]/gu, `\\$&`)
 
-    // add the escaped substitution to the set of substitutions to make
-    subs.set(escapedInput, replacement);
+      // add the escaped substitution to the set of substitutions to make
+      subs.set(escapedInput, replacement)
 
-    // check for feeding problems, and create temporary placeholder substitutions if found
-    if (inputs.includes(replacement)) {
+      // check for feeding problems, and create temporary placeholder substitutions if found
+      if (inputs.includes(replacement)) {
 
-      // get a random temporary placeholder to substitute
-      let temp = getRandomCodePoint();
+        // get a random temporary placeholder to substitute
+        let temp = getRandomCodePoint()
 
-      // make sure you haven't already used that placeholder, and generate a new one if so
-      while (temps.has(temp)) temp = getRandomCodePoint();
+        // make sure you haven't already used that placeholder, and generate a new one if so
+        while (temps.has(temp)) temp = getRandomCodePoint()
 
-      // add the placeholder to the set of temporary substitutions
-      temps.set(temp, replacement);
+        // add the placeholder to the set of temporary substitutions
+        temps.set(temp, replacement)
 
-      // update the list of substitutions to use the temporary placeholder
-      subs.set(escapedInput, temp);
+        // update the list of substitutions to use the temporary placeholder
+        subs.set(escapedInput, temp)
 
-    }
+      }
 
-    // make the substitution on the string, using the temporary placeholder if present
-    const regexp = new RegExp(escapedInput, `gu`);
-    str = str.replace(regexp, subs.get(escapedInput));
+      // make the substitution on the string, using the temporary placeholder if present
+      const regexp = new RegExp(escapedInput, `gu`)
+      str = str.replace(regexp, subs.get(escapedInput))
 
-  });
+    })
 
   // replace the temporary placeholders with their original values
   temps.forEach((replacement, temp) => {
-    const regexp = new RegExp(temp, `gu`);
-    str = str.replace(regexp, replacement);
-  });
+    const regexp = new RegExp(temp, `gu`)
+    str = str.replace(regexp, replacement)
+  })
 
   // return the transliterated string
-  return str;
+  return str
 
 }
 
@@ -141,8 +142,8 @@ class Transliterator {
    * @return {Function}               Returns a transliterate function that accepts a string and makes the substitutions provided in the `transliterate` argument.
    */
   constructor(substitutions) {
-    this.substitutions = substitutions;
-    return string => transliterate(string, this.substitutions);
+    this.substitutions = substitutions
+    return string => transliterate(string, this.substitutions)
   }
 }
 
@@ -153,4 +154,4 @@ export {
   Sanitizer,
   transliterate,
   Transliterator,
-};
+}
